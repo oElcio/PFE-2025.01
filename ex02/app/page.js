fb9dd27f0d8ca510e58b90e11c57d0ffee89b97a
elcio <elcioferreira398@gmail.com>
2025-03-14 02:37:13 +0000

    quase terminando a atividade 2

--- ex02/app/page.js
+++ ex02/app/page.js
@@ -1,9 +1,25 @@
-import styles from "./page.module.css";
 "use client"
 
+import { useState } from "react";
+import styles from "./page.module.css";
+import { Dado } from "@/components/Dado";
+
 export default function Home() {
+
+  function randowNumber(){
+    setCount(Math.floor(Math.random() * 6) + 1);
+  }
+  
+  const [count, setCount] = useState(0);
+
   return (
     <div>
+
+      <button onClick={ randowNumber } className={styles.button}>
+          Clique aqui!! 
+        </button>
+
+        <Dado valor={ count } className={styles.dado}/>
       
     </div>
   );
