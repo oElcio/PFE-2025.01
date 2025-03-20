430a66a6a5a331b9b7eb15a0ff35e52361de833f
elcio <elcioferreira398@gmail.com>
2025-03-20 22:06:33 +0000

    finalizando a tarefa 03

--- ex03/app/page.js
+++ ex03/app/page.js
@@ -1,94 +1,86 @@
 "use client"
 
 import { useState } from "react";
-import styles from "./page.module.css";
+import { Dado } from "../component/Dado/Dado";
+import './Styles.css';
 
 export default function Home() {
 
-  function rolar_dado(){
-    Math.floor(Math.random() * 6) + 1;
+  const [rounds, setRounds] = useState(1);
+  const [counters, setCounters] = useState([[], []]);
+  const [current, setCurrent] = useState(0);
+
+  const points = counters
+    .map((counter) => counter.reduce((a, b) => a + b, 0))
+    .sort((a, b) => b - a)
+    .map((points, i) => ({
+      points,
+      player: i,
+    }));
+
+  function randomNumber(index) {
+    if (rounds === 5) return;
+
+    const random = Math.floor(Math.random() * 6) || 1;
+
+    const copy = [...counters];
+    copy[index].push(random);
+
+    setCounters(copy);
+    setCurrent(current === counters.length - 1 ? 0 : current + 1);
+
+    if (copy.every((counter) => counter.length === counters[0].length)) {
+      setRounds(rounds + 1);
+    }
   }
 
-  const App = () => {
-    const [rodada, setRodada] = useState(1); // Número da rodada atual
-    const [pontuacaoJogador1, setPontuacaoJogador1] = useState(0); // Pontuação do jogador 1
-    const [pontuacaoJogador2, setPontuacaoJogador2] = useState(0); // Pontuação do jogador 2
-    const [vencedor, setVencedor] = useState(null); // Quem venceu o jogo
-    const [resultadoRodada, setResultadoRodada] = useState(""); // Resultado da rodada atual
-    const [jogoAtivo, setJogoAtivo] = useState(true); // Estado que controla se o jogo está ativo
-  
-    // Função para iniciar a próxima rodada
-    const jogarRodada = () => {
-      if (rodada <= 5 && jogoAtivo) {
-        // Rolar o dado para os dois jogadores
-        const dadoJogador1 = rolarDado();
-        const dadoJogador2 = rolarDado();
-  
-        // Determinar quem ganhou a rodada
-        let vencedorRodada = "";
-        if (dadoJogador1 > dadoJogador2) {
-          setPontuacaoJogador1(pontuacaoJogador1 + 1);
-          vencedorRodada = "Jogador 1 venceu a rodada!";
-        } else if (dadoJogador2 > dadoJogador1) {
-          setPontuacaoJogador2(pontuacaoJogador2 + 1);
-          vencedorRodada = "Jogador 2 venceu a rodada!";
-        } else {
-          vencedorRodada = "Empate!";
-        }
-  
-        // Avançar para a próxima rodada
-        setResultadoRodada(vencedorRodada);
-        setRodada(rodada + 1);
-  
-        // Verificar se o jogo acabou
-        if (rodada === 5) {
-          if (pontuacaoJogador1 > pontuacaoJogador2) {
-            setVencedor("Jogador 1 venceu o jogo!");
-          } else if (pontuacaoJogador2 > pontuacaoJogador1) {
-            setVencedor("Jogador 2 venceu o jogo!");
-          } else {
-            setVencedor("O jogo empatou!");
-          }
-          setJogoAtivo(false);
-        }
-      }
-    };
-  
-    // Função para reiniciar o jogo
-    const reiniciarJogo = () => {
-      setRodada(1);
-      setPontuacaoJogador1(0);
-      setPontuacaoJogador2(0);
-      setVencedor(null);
-      setResultadoRodada("");
-      setJogoAtivo(true);
-    };
-  
   return (
-    <div className="app">
-      <h1>Jogo de Dados</h1>
-      <div className="jogo">
-        <div className="status">
-          <p>Rodada: {rodada}/5</p>
-          <p>Jogador 1: {pontuacaoJogador1} pontos</p>
-          <p>Jogador 2: {pontuacaoJogador2} pontos</p>
-        </div>
+    <div>
+      <h1>Rounds: {rounds}</h1>
 
-        <div className="resultado">
-          <p>{resultadoRodada}</p>
-          {vencedor && <h2>{vencedor}</h2>}
-        </div>
+      <div className="dados">
+        {counters.map((counter, i) => (
+          <div key={`counter-${i}`}>
+            <h2>Jogador {i + 1}</h2>
+            <Dado valor={counter.slice(-1)[0]} />
 
-        <div className="acoes">
-          {jogoAtivo ? (
-            <button onClick={jogarRodada}>Jogar Rodada</button>
-          ) : (
-            <button onClick={reiniciarJogo}>Jogar Novamente!</button>
-          )}
-        </div>
+            <button
+              variant="outline"
+              disabled={i !== current || rounds === 5}
+              onClick={() => randomNumber(i)}
+            >
+              Clique aqui
+            </button>
+          </div>
+        ))}
       </div>
+
+      {rounds === 5 && (
+        <div>
+          <p>Jogo terminado!</p>
+
+          <p>
+            Melhor pontuação: {points[0].points} por Jogador {points[0].player + 1}
+          </p>
+
+          <div>
+            {points.map((point, i) => (
+              <p key={`point-${i}`}>
+                {point.points} por Jogador {point.player + 1}
+              </p>
+            ))}
+          </div>
+
+          <button
+            onClick={() => {
+              setRounds(1);
+              setCounters([[], []]);
+            }}
+          >
+            Reiniciar
+          </button>
+        </div>
+      )}
     </div>
   );
-};
-
-export default App; }
+} 
---
+++ ex03/component/Dado/Dado.jsx
@@ -0,0 +1,32 @@
+import styles from "./Dado.module.css"
+export function Dado(props){
+    
+    let url;
+    
+    switch(props.valor){
+        case 1:
+            url = "https://www.svgrepo.com/show/499118/dice-one.svg";
+            break;
+        case 2:
+            url = "https://www.svgrepo.com/show/499122/dice-two.svg";
+            break;
+        case 3:
+            url = "https://www.svgrepo.com/show/499121/dice-three.svg";
+            break;
+        case 4:
+            url = "https://www.svgrepo.com/show/499119/dice-four.svg";
+            break;
+        case 5:
+            url = "https://www.svgrepo.com/show/499117/dice-five.svg";
+            break;
+        case 6:
+            url = "https://www.svgrepo.com/show/499120/dice-six.svg";
+            break;
+
+    }
+    return (
+        <div> 
+            <img src={url} alt="" className={styles.Dado}/>
+        </div>
+    )
+}
